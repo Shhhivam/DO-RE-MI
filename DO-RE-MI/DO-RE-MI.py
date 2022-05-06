fe6b8537010f74b9a0e6d6c450d88893011a2e96
Shivam Gupta <shivamsg65@gmail.com>
2022-05-06 16:42:43 +0530

    Add files via upload

---
+++ DO-RE-MI/DO-RE-MI.py
@@ -0,0 +1,10 @@
+
+
+
+
+if __name__ == "__main__":
+    print("Welcome to the best multi musical intrument place")
+    print("For Xylophone press1\n For Piano press2\nFor Drum press3")
+    user_input = int(input("Enter the number"))
+    print(user_input)
+    
